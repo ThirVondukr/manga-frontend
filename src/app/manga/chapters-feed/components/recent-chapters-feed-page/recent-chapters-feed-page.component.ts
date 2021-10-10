5e690b635791bce3acb4c7bdaf5d7736648a9e64
Doctor <thirvondukr@gmail.com>
2021-10-10 11:09:57 +0300

    Change service access modifier to private

--- src/app/manga/chapters-feed/components/recent-chapters-feed-page/recent-chapters-feed-page.component.ts
+++ src/app/manga/chapters-feed/components/recent-chapters-feed-page/recent-chapters-feed-page.component.ts
@@ -18,7 +18,7 @@ export class RecentChaptersFeedPageComponent extends InfiniteScrollBase<RecentCh
     private readonly _PAGE_SIZE = 20
 
     constructor(
-        public readonly _chaptersService: ChaptersFeedService,
+        private readonly _chaptersService: ChaptersFeedService,
         _scroll: InfiniteScrollService,
     ) {
         super(_scroll);
@@ -37,5 +37,4 @@ export class RecentChaptersFeedPageComponent extends InfiniteScrollBase<RecentCh
     protected mapCursor = (res: Response) => res.pageInfo.endCursor;
     protected mapHasNextPage = (response: Response) => response.pageInfo.hasNextPage;
     protected mapItems = (response: Observable<Response>) => ChaptersFeedService.createChapterGroups(response);
-
 }
