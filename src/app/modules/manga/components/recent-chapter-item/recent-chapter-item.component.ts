1e9566577fc6ec2c0ca1b77254c72fdde33258e3
Doctor <thirvondukr@gmail.com>
2021-11-21 00:48:06 +0300

    Add recent chapters component

--- src/app/modules/manga/components/recent-chapter-item/recent-chapter-item.component.ts
+++ src/app/modules/manga/components/recent-chapter-item/recent-chapter-item.component.ts
@@ -1,22 +1,40 @@
 import {ChangeDetectionStrategy, Component, Input} from "@angular/core";
 
-import {MangaHome_recentChapters_edges_node} from "src/gql/generated/MangaHome";
-import {MangaRoutingService} from "src/app/routing";
 
+interface Chapter {
+    id: any;
+    title: string;
+    volume: number;
+    number: number;
+    numberExtra: number;
+    publishedAt: string;
+}
+
+interface Manga {
+    id: any;
+    title: string;
+}
+
+interface TranslatorsGroup {
+    title: string;
+}
 
 @Component({
-    selector: "app-recent-chapter-item[chapter]",
+    selector: "app-recent-chapter-item",
     templateUrl: "./recent-chapter-item.component.html",
     styleUrls: ["./recent-chapter-item.component.scss"],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecentChapterItemComponent {
+    @Input()
+    chapter!: Chapter;
 
     @Input()
-    public chapter!: MangaHome_recentChapters_edges_node;
+    manga!: Manga;
 
-    constructor(
-        public readonly mangaRouting: MangaRoutingService,
-    ) {
-    }
+    @Input()
+    translatorsGroup!: TranslatorsGroup;
+
+    @Input()
+    imageUrl!: string;
 }
